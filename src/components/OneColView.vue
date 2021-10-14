f8e88cf9fb5049256a439b4ec76d52d88980f80e
KenOHD <od-r.35107@i.softbank.jp>
2021-10-15 01:47:18 +0900

    1col-view-page_add

---
+++ src/components/OneColView.vue
@@ -0,0 +1,23 @@
+<template>
+    <el-container>
+      <el-header style="background-color: #FFFFFF">
+        <Header></Header>
+      </el-header>
+      <el-container>
+        <router-view />
+      </el-container>
+    </el-container>
+</template>
+
+<script lang="ts">
+  import Header from './general/Header.vue'
+  export default {
+    name: 'App',
+    components: {
+      Header,
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+</style>
